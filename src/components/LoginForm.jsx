f4aa3adc96adb3a482bb0fae56b52409e67371af
vikramsardiwal <vikramsardiwal22@gmail.com>
2026-08-03 11:55:03 +0530

    feat: clear mobile number input after submission

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -12,6 +12,7 @@ export default function LoginForm() {
   function handlesubmit() {
     if (number.length === 10) {
       setError("");
+      setNumber("");
       console.log("success");
     } else {
       setError("pls valid 10 digit number");
